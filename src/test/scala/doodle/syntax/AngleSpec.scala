1629a7d82276dadc81786beb2deb5e81c73c45b7
Noel Welsh <noel@untyped.com>
2015-01-01 10:35:50 +0000

    Test the angle syntax
    This project is getting big enough that it needs more testing. Start
    being more systematic

---
+++ src/test/scala/doodle/syntax/AngleSpec.scala
@@ -0,0 +1,14 @@
+package doodle
+
+import utest._
+import doodle.syntax.angle._
+
+object AngleSpec extends TestSuite {
+  val tests = TestSuite {
+    "syntax constructs expected angles"-{
+      assert(60.degrees == Angle.degrees(60))
+      assert(1.radians == Angle.radians(1))
+      assert(0.5.turns == Angle.turns(0.5))
+    }
+  }
+}
