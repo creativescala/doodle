ab0f5803d907c75b08b9c5c8c85900332972c101
Jonathan Ferguson <jono@spiralarm.com>
2015-02-12 13:13:10 +1100

    Fix compilation error in js/Main

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -6,8 +6,8 @@ import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
 
 @JSExport object Main extends JSApp {
-  val image = ChristmasTree
-  val anim  = Orbit(Angle.radians(0), 50, image)
+  val image:Image = ChristmasTree
+  val anim  = Orbit(image,50,Angle.radians(0))
 
   @JSExport def main(): Unit =
     doodle.js.animate(anim, "canvas")
