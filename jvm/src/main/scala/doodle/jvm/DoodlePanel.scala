4c62a7e883ecfa0fa2e872741e9356fbf169098f
Jonathan Ferguson <jono@spiralarm.com>
2015-02-12 17:33:27 +1100

    Add the Path case class
    This extends Image and takes one or more PathElements.

--- jvm/src/main/scala/doodle/jvm/DoodlePanel.scala
+++ jvm/src/main/scala/doodle/jvm/DoodlePanel.scala
@@ -2,8 +2,8 @@ package doodle.jvm
 
 import doodle.core._
 
-import java.awt.{Color => AwtColor, BasicStroke, Dimension, Graphics, Graphics2D, RenderingHints, Shape}
-import java.awt.geom.{Ellipse2D, Path2D, Rectangle2D}
+import java.awt.{ Color ⇒ AwtColor, BasicStroke, Dimension, Graphics, Graphics2D, RenderingHints, Shape }
+import java.awt.geom.{ Ellipse2D, Path2D, Rectangle2D }
 import javax.swing.JPanel
 
 object DoodlePanel {
@@ -11,7 +11,7 @@ object DoodlePanel {
     new DoodlePanel(image)
 }
 
-class DoodlePanel private(private var _image: Image) extends JPanel {
+class DoodlePanel private (private var _image: Image) extends JPanel {
   updateBounds()
 
   def image: Image =
@@ -28,8 +28,7 @@ class DoodlePanel private(private var _image: Image) extends JPanel {
     val bounds = BoundingBox(image)
     setPreferredSize(new Dimension(
       bounds.width.toInt + 2 * margin,
-      bounds.height.toInt + 2 * margin
-    ))
+      bounds.height.toInt + 2 * margin))
   }
 
   override def paintComponent(graphics: Graphics): Unit = {
@@ -37,23 +36,22 @@ class DoodlePanel private(private var _image: Image) extends JPanel {
 
     ctx.setRenderingHints(new RenderingHints(
       RenderingHints.KEY_ANTIALIASING,
-      RenderingHints.VALUE_ANTIALIAS_ON
-    ))
+      RenderingHints.VALUE_ANTIALIAS_ON))
 
-    paintImage(image, Point(getWidth/2, getHeight/2), DrawingContext.blackLines)
+    paintImage(image, Point(getWidth / 2, getHeight / 2), DrawingContext.blackLines)
   }
 
   def paintImage(image: Image, origin: Point, context: DrawingContext)(implicit g: Graphics2D): Unit = {
     def strokeAndFill(shape: Shape) = {
       context.fill.foreach {
-        case Fill(color) => {
+        case Fill(color) ⇒ {
           g.setPaint(awtColor(color))
           g.fill(shape)
         }
       }
 
       context.stroke.foreach {
-        case Stroke(width, color, cap, join) => {
+        case Stroke(width, color, cap, join) ⇒ {
           g.setStroke(new BasicStroke(width.toFloat))
           g.setPaint(awtColor(color))
           g.draw(shape)
@@ -62,38 +60,38 @@ class DoodlePanel private(private var _image: Image) extends JPanel {
     }
 
     image match {
-      case Circle(r) =>
-        strokeAndFill(new Ellipse2D.Double(origin.x-r, origin.y-r, r*2, r*2))
+      case Circle(r) ⇒
+        strokeAndFill(new Ellipse2D.Double(origin.x - r, origin.y - r, r * 2, r * 2))
 
-      case Rectangle(w, h) =>
-        strokeAndFill(new Rectangle2D.Double(origin.x - w/2, origin.y - h/2, w, h))
+      case Rectangle(w, h) ⇒
+        strokeAndFill(new Rectangle2D.Double(origin.x - w / 2, origin.y - h / 2, w, h))
 
-      case Triangle(w, h) =>
+      case Triangle(w, h) ⇒
         val path = new Path2D.Double()
-        path.moveTo(origin.x       , origin.y - h/2)
-        path.lineTo(origin.x + w/2 , origin.y + h/2)
-        path.lineTo(origin.x - w/2 , origin.y + h/2)
-        path.lineTo(origin.x       , origin.y - h/2)
+        path.moveTo(origin.x, origin.y - h / 2)
+        path.lineTo(origin.x + w / 2, origin.y + h / 2)
+        path.lineTo(origin.x - w / 2, origin.y + h / 2)
+        path.lineTo(origin.x, origin.y - h / 2)
         strokeAndFill(path)
 
-      case Overlay(t, b) =>
+      case Overlay(t, b) ⇒
         paintImage(b, origin, context)
         paintImage(t, origin, context)
 
-      case b @ Beside(l, r) =>
-        val box  = BoundingBox(b)
+      case b @ Beside(l, r) ⇒
+        val box = BoundingBox(b)
         val lBox = BoundingBox(l)
         val rBox = BoundingBox(r)
 
-        val lOriginX = origin.x + box.left  + (lBox.width / 2)
+        val lOriginX = origin.x + box.left + (lBox.width / 2)
         val rOriginX = origin.x + box.right - (rBox.width / 2)
         // Beside always vertically centers l and r, so we don't need
         // to calculate center ys for l and r.
 
         paintImage(l, Point(lOriginX, origin.y), context)
         paintImage(r, Point(rOriginX, origin.y), context)
-      case a @ Above(t, b) =>
-        val box  = BoundingBox(a)
+      case a @ Above(t, b) ⇒
+        val box = BoundingBox(a)
         val tBox = BoundingBox(t)
         val bBox = BoundingBox(b)
 
@@ -102,14 +100,26 @@ class DoodlePanel private(private var _image: Image) extends JPanel {
 
         paintImage(t, Point(origin.x, tOriginY), context)
         paintImage(b, Point(origin.x, bOriginY), context)
-      case At(vec, i) =>
+      case At(vec, i) ⇒
         paintImage(i, origin + vec, context)
 
-      case ContextTransform(f, i) =>
+      case ContextTransform(f, i) ⇒
         paintImage(i, origin, f(context))
 
-      case d: Drawable =>
+      case d: Drawable ⇒
         paintImage(d.draw, origin, context)
+      case _path @ Path(head, tail) ⇒
+        val box = BoundingBox(_path)
+        val path = new Path2D.Double()
+        path.moveTo(head.x, head.y)
+        tail.foreach {
+          case MoveTo(x, y)                        ⇒ path.moveTo(x, y)
+          case LineTo(x, y)                        ⇒ path.lineTo(x, y)
+          case ArcTo(x, y, x2, y2, r)              ⇒ path.curveTo(x, y, x2, y2, r, 0) //Wrong
+          case QuadraticCurveTo(cpx, cpy, x, y)    ⇒ path.quadTo(cpx, cpy, x, y)
+          case BezierCurveTo(x, y, x2, y2, x3, y3) ⇒ path.curveTo(x, y, x2, y2, x3, y3) //Wrong          
+        }
+        strokeAndFill(path)
     }
   }
 
--- shared/src/main/scala/doodle/core/BoundingBox.scala
+++ shared/src/main/scala/doodle/core/BoundingBox.scala
@@ -1,13 +1,13 @@
 package doodle.core
 
 /**
-  * Represents a bounding box around an image. The coordinate system
-  * follows the canvas convention, meaning that the origin is the top
-  * left. However, the coordinate system is not the global coordinate
-  * system of the canvas but one that is local to the image. We
-  * require the image is centered at the origin, and thus left and top
-  * will usually be negative.
-  */
+ * Represents a bounding box around an image. The coordinate system
+ * follows the canvas convention, meaning that the origin is the top
+ * left. However, the coordinate system is not the global coordinate
+ * system of the canvas but one that is local to the image. We
+ * require the image is centered at the origin, and thus left and top
+ * will usually be negative.
+ */
 final case class BoundingBox(left: Double, top: Double, right: Double, bottom: Double) {
   // TODO: Are bounding boxes ever not symmetric around any given
   // axis? Can we replace l/t/r/b with just width and height?
@@ -20,31 +20,30 @@ final case class BoundingBox(left: Double, top: Double, right: Double, bottom: D
 
 object BoundingBox {
   def apply(image: Image): BoundingBox = image match {
-    case Circle(r) =>
+    case Circle(r) ⇒
       BoundingBox(-r, -r, r, r)
 
-    case Rectangle(w, h) =>
-      BoundingBox(-w/2, -h/2, w/2, h/2)
+    case Rectangle(w, h) ⇒
+      BoundingBox(-w / 2, -h / 2, w / 2, h / 2)
 
-    case Triangle(w, h) =>
-      BoundingBox(-w/2, -h/2, w/2, h/2)
+    case Triangle(w, h) ⇒
+      BoundingBox(-w / 2, -h / 2, w / 2, h / 2)
 
-    case Overlay(t, b) =>
+    case Overlay(t, b) ⇒
       val BoundingBox(l1, t1, r1, b1) = BoundingBox(t)
       val BoundingBox(l2, t2, r2, b2) = BoundingBox(b)
       BoundingBox(l1 min l2, t1 min t2, r1 max r2, b1 max b2)
 
-    case Beside(l, r) =>
+    case Beside(l, r) ⇒
       val boxL = BoundingBox(l)
       val boxR = BoundingBox(r)
       BoundingBox(
         -(boxL.width + boxR.width) / 2,
         boxL.top min boxR.top,
         (boxL.width + boxR.width) / 2,
-        boxL.bottom max boxR.bottom
-      )
+        boxL.bottom max boxR.bottom)
 
-    case Above(t, b) =>
+    case Above(t, b) ⇒
       val boxT = BoundingBox(t)
       val boxB = BoundingBox(b)
 
@@ -52,10 +51,9 @@ object BoundingBox {
         boxT.left min boxB.left,
         -(boxT.height + boxB.height) / 2,
         boxT.right max boxB.right,
-        (boxT.height + boxB.height) / 2
-      )
+        (boxT.height + boxB.height) / 2)
 
-    case At(Vec(x, y), i) =>
+    case At(Vec(x, y), i) ⇒
       val inner = BoundingBox(i)
       // Because bounding boxes are required to be centered on the the
       // image they enclose, all we need to do is divide the x and y
@@ -64,16 +62,37 @@ object BoundingBox {
       val xAmount = math.abs(x) / 2
       val yAmount = math.abs(y) / 2
       BoundingBox(
-        inner.left   - xAmount,
-        inner.top    - yAmount,
-        inner.right  + xAmount,
-        inner.bottom + yAmount
-      )
+        inner.left - xAmount,
+        inner.top - yAmount,
+        inner.right + xAmount,
+        inner.bottom + yAmount)
 
-    case ContextTransform(f, i) =>
+    case ContextTransform(f, i) ⇒
       BoundingBox(i)
 
-    case d: Drawable =>
+    case d: Drawable ⇒
       BoundingBox(d.draw)
+
+    case Path(head, tail) ⇒
+     
+      implicit class bounder(b: BoundingBox) {
+        def bound(x: Double, y: Double): BoundingBox = {
+          b.copy(left = Math.min(b.top, x),
+            top = Math.min(b.top, y),
+            right = Math.max(b.top, x),
+            bottom = Math.max(b.top, y))
+        }
+      }
+      val box: BoundingBox = BoundingBox(0, 0, 0, 0).bound(head.x,head.y)
+    
+      tail.foldLeft(box) { (b, e) ⇒
+        e match {
+          case MoveTo(x, y)                        ⇒ box.bound(x, y)
+          case LineTo(x, y)                        ⇒ box.bound(x, y)
+          case ArcTo(x, y, x2, y2, r)              ⇒ box.bound(x, y).bound(x2, y2)
+          case QuadraticCurveTo(cpx, cpy, x, y)    ⇒ box.bound(x, y).bound(cpx, cpy)
+          case BezierCurveTo(x, y, x2, y2, x3, y3) ⇒ box.bound(x, y).bound(x2, y2).bound(x3, y3)
+        }
+      }
   }
 }
