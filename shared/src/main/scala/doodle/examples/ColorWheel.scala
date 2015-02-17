270727accc1cf86361c0d87026a67895571ad0f1
Dave Gurnell <dave@untyped.com>
2015-02-17 17:19:59 +0000

    Updated `ColorWheel` example to include lightness spectrum

--- shared/src/main/scala/doodle/examples/ColorWheel.scala
+++ shared/src/main/scala/doodle/examples/ColorWheel.scala
@@ -4,12 +4,16 @@ import doodle.core._
 import doodle.syntax._
 
 object ColorWheel extends Drawable {
-  val blobs = (0 to 360 by 10).map { hue =>
-    val angle = Angle.degrees(hue)
+  val blobs = for {
+    l <- (0 to 100 by 10) map (_ / 100.0)
+    h <- (0 to 360 by 10)
+    r  = 200 * l
+    a  = Angle.degrees(h)
+  } yield {
     Circle(20).
-      at(50 * angle.sin, 50 * angle.cos).
+      at(r * a.sin, r * a.cos).
       lineWidth(0).
-      fillColor(Color.hsl(hue, 1, 0.5))
+      fillColor(Color.hsl(h, 1, l))
   }
 
   def draw = blobs.reduceLeft(_ on _)
