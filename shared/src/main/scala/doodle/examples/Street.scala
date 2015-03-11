a102d75fe66caed98c60c266c169d482f42d333a
Dave Gurnell <dave@untyped.com>
2015-03-11 11:24:35 +0000

    Added Street example

---
+++ shared/src/main/scala/doodle/examples/Street.scala
@@ -0,0 +1,41 @@
+package doodle.examples
+
+import doodle.core._
+import doodle.syntax._
+
+object Street extends Drawable {
+  import Color._
+
+  val roof = Triangle(50, 30) fillColor brown
+
+  val frontDoor =
+    (Rectangle(50, 15) fillColor red) above (
+      (Rectangle(10, 25) fillColor black) on
+      (Rectangle(50, 25) fillColor red)
+    )
+
+  val house = roof above frontDoor
+
+  val tree =
+    (Circle(25) fillColor green) above
+    (Rectangle(10, 20) fillColor brown)
+
+  val street =
+    allBeside(
+      (0 to 105 by 45) map { i =>
+        (Rectangle(30, 3) fillColor yellow) beside
+        (Rectangle(15, 3) fillColor black) above
+        (Rectangle(45, 7) fillColor black)
+      }
+    )
+
+
+  val houseAndGarden =
+    (house beside tree) above street
+
+  val draw = (
+    houseAndGarden beside
+    houseAndGarden beside
+    houseAndGarden
+  ) lineWidth 0
+}
