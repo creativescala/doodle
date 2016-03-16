64e7e7801442561264b78128dfb8ecc4476f8a41
Noel Welsh <noel@untyped.com>
2016-03-16 13:16:05 +0000

    Correct handling of coordinates transform
    Previous conversion from canvas to screen coordinates was incorrect. Fix
    this.

--- js/src/main/scala/doodle/js/HtmlCanvas.scala
+++ js/src/main/scala/doodle/js/HtmlCanvas.scala
@@ -1,28 +1,27 @@
 package doodle
 package js
 
-import doodle.core.{Color, Stroke}
+import doodle.core.{Color, Stroke, Vec}
 import doodle.backend.Canvas
 
 import org.scalajs.dom
 
 class HtmlCanvas(canvas: dom.raw.HTMLCanvasElement) extends Canvas {
   val context = canvas.getContext("2d").asInstanceOf[dom.raw.CanvasRenderingContext2D]
-  var originX = canvas.width / 2
-  var originY = canvas.height / 2
   var animationFrameCallbackHandle: Option[Int] = None
 
-  /** Transform from Canvas coordinates to HTMLCanvasElement coordinates */
-  def transformX(x: Double): Double =
-    originX + x
-
-  /** Transform from Canvas coordinates to HTMLCanvasElement coordinates */
-  def transformY(y: Double): Double =
-    originY - y
+  // The origin in canvas coordinates
+  var center = Vec(0, 0)
+  // Convert from canvas coordinates to screen coordinates
+  def canvasToScreen(x: Double, y: Double): Vec = {
+    val offsetX = canvas.width / 2
+    val offsetY = canvas.height / 2
+    val Vec(centerX, centerY) = center
+    Vec(x - centerX + offsetX, offsetY - y + centerY)
+  }
 
   def setOrigin(x: Int, y: Int): Unit = {
-    originX = (canvas.width / 2) + x
-    originY = (canvas.height / 2) + y
+    center = Vec(x, y)
   }
 
   def clear(color: Color): Unit = {
@@ -57,17 +56,24 @@ class HtmlCanvas(canvas: dom.raw.HTMLCanvasElement) extends Canvas {
   def beginPath(): Unit =
     context.beginPath()
 
-  def moveTo(x: Double, y: Double): Unit =
-    context.moveTo(transformX(x), transformY(y))
+  def moveTo(x: Double, y: Double): Unit = {
+    val Vec(screenX, screenY) = canvasToScreen(x, y)
+    context.moveTo(screenX, screenY)
+  }
 
-  def lineTo(x: Double, y: Double): Unit =
-    context.lineTo(transformX(x), transformY(y))
+  def lineTo(x: Double, y: Double): Unit = {
+    val Vec(screenX, screenY) = canvasToScreen(x, y)
+    context.lineTo(screenX, screenY)
+  }
 
   def bezierCurveTo(cp1x: Double, cp1y: Double, cp2x: Double, cp2y: Double, endX: Double, endY: Double): Unit = {
+    val Vec(screenCp1X, screenCp1Y) = canvasToScreen(cp1x, cp1y)
+    val Vec(screenCp2X, screenCp2Y) = canvasToScreen(cp2x, cp2y)
+    val Vec(screenEndX, screenEndY) = canvasToScreen(endX, endY)
     context.bezierCurveTo(
-      transformX(cp1x), transformY(cp1y),
-      transformX(cp2x), transformY(cp2y),
-      transformX(endX), transformY(endY)
+      screenCp1X , screenCp1Y,
+      screenCp2X , screenCp2Y,
+      screenEndX , screenEndY
     )
   }
 
