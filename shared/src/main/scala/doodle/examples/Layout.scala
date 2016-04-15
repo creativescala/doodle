9a39af55bcc3e4879be2ec1854a7e9df46913030
Noel Welsh <noel@Noels-MacBook-Pro.local>
2016-04-15 21:38:27 +0100

    Illustrate how origins are used in layout

---
+++ shared/src/main/scala/doodle/examples/Layout.scala
@@ -0,0 +1,39 @@
+package doodle
+package examples
+
+import doodle.core._
+import doodle.syntax._
+
+object Layout {
+  // Examples for debugging layout
+
+  import doodle.core.Image._
+  import doodle.core.Point._
+  import doodle.core.Color._
+
+  def addOrigin(image: Image): Image = {
+    val origin = Circle(5).noLine.fillColor(Color.red)
+    origin on image
+  }
+
+  // Examples of paths that are not centered in their bounding box
+  val triangle =
+    addOrigin(
+      openPath(List(
+                 LineTo(cartesian(50, 100)),
+                 LineTo(cartesian(100, 0)),
+                 LineTo(cartesian(0, 0))
+               ))
+    )
+
+  val curve =
+    addOrigin(
+      openPath(
+        List(BezierCurveTo(cartesian(50, 100), cartesian(100, 100), cartesian(150, 0)))
+      )
+    )
+
+
+  val vertical = triangle above (addOrigin(circle(100))) above curve
+  val horizontal = triangle beside (addOrigin(circle(100))) beside curve
+}
