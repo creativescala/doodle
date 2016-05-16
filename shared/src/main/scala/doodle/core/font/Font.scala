fcaa7e3a19bcb8da50a8f1787e6a32fa7c94dc1c
Noel Welsh <noel@Noels-MacBook-Pro.local>
2016-05-16 10:58:10 +0100

    Package ADT members

--- shared/src/main/scala/doodle/core/font/Font.scala
+++ shared/src/main/scala/doodle/core/font/Font.scala
@@ -3,22 +3,43 @@ package core
 package font
 
 sealed abstract class FontFamily extends Product with Serializable
-final case object Serif extends FontFamily
-final case object SansSerif extends FontFamily
-final case object Monospaced extends FontFamily
-final case class Named(get: String) extends FontFamily
+object FontFamily {
+  final case object Serif extends FontFamily
+  final case object SansSerif extends FontFamily
+  final case object Monospaced extends FontFamily
+  final case class Named(get: String) extends FontFamily
+
+  val serif: FontFamily = Serif
+  val sansSerif: FontFamily = SansSerif
+  val monospaced: FontFamily = Monospaced
+  def named(name: String): FontFamily = Named(name)
+}
 
 sealed abstract class FontFace extends Product with Serializable
-final case object Bold extends FontFace
-final case object Italic extends FontFace
-final case object Normal extends FontFace
+object FontFace {
+  final case object Bold extends FontFace
+  final case object Italic extends FontFace
+  final case object Normal extends FontFace
+
+  val bold: FontFace = Bold
+  val italic: FontFace = Italic
+  val normal: FontFace = Normal
+}
 
 sealed abstract class FontSize extends Product with Serializable
-final case class Points(get: Int) extends FontSize
+object FontSize {
+  final case class Points(get: Int) extends FontSize
+
+  def points(pts: Int): FontSize = Points(pts)
+}
 
 
 final case class Font(family: FontFamily, face: FontFace, size: FontSize)
 object Font {
-  val defaultSerif = Font(Serif, Normal, Points(12))
-  val defaultSansSerif = Font(SansSerif, Normal, Points(12))
+  import FontFamily._
+  import FontFace._
+  import FontSize._
+
+  val defaultSerif = Font(serif, normal, points(12))
+  val defaultSansSerif = Font(sansSerif, normal, points(12))
 }
