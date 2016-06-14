47f5da0c772c8c388211cdf29f00f16af056cb4b
Noel Welsh <noel@Noels-MacBook-Pro.local>
2016-06-14 13:15:13 +0100

    Example of a braching / growing process

--- shared/src/main/scala/doodle/examples/CreativeScala.scala
+++ shared/src/main/scala/doodle/examples/CreativeScala.scala
@@ -398,4 +398,60 @@ object CreativeScala {
         }
       )
   }
+
+  object turtle {
+    import doodle.turtle._
+    import doodle.turtle.Instruction._
+
+    val instructions =
+      List(forward(100), turn(90.degrees),
+           forward(100), turn(90.degrees),
+           forward(100), turn(90.degrees),
+           forward(100))
+
+    val square = Turtle.draw(instructions)
+
+    def polygon(sides: Int, sideLength: Double): Image = {
+      val rotation = Angle.one / sides
+      def iter(n: Int): List[Instruction] =
+        n match {
+          case 0 => Nil
+          case n => turn(rotation) +: forward(sideLength) +: iter(n-1)
+        }
+
+      Turtle.draw(iter(sides))
+    }
+  }
+
+  object branching {
+    import doodle.turtle._
+    import doodle.turtle.Instruction._
+
+    val y = List(forward(100),
+                 branch(turn(45.degrees), forward(100)),
+                 branch(turn(-45.degrees), forward(100)))
+
+    val yImage = Turtle.draw(y)
+
+    val bud = branch(turn(45.degrees), forward(5),
+                     turn(-90.degrees), forward(5),
+                     turn(-90.degrees), forward(5),
+                     turn(-90.degrees), forward(5))
+
+    val f = forward(20)
+
+    val b1 = branch(turn(45.degrees), f)
+    val b2 = branch(turn(-45.degrees), f)
+
+    val treeOne = List(f, bud)
+    val treeTwo = List(f, f, b1 :+ (bud), b2 :+ (bud))
+    val treeThree = List(f, f, f,
+                         b1 ++ (List(f, b1 :+ bud, b2 :+ (bud))),
+                         b2 ++ (List(f, b1 :+ bud, b2 :+ (bud))))
+
+    val spacer = rectangle(10,10).noFill.noLine
+
+    val branches =
+      Turtle.draw(treeOne) beside spacer beside Turtle.draw(treeTwo) beside spacer beside Turtle.draw(treeThree) lineColor Color.forestGreen
+  }
 }
