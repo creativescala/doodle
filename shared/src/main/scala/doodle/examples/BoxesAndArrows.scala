aab5875bb6842d3fa91598b260e4f13e45ed3f49
Noel Welsh <noel@Noels-MacBook-Pro.local>
2016-06-15 20:56:23 +0100

    flatMap box and arrow diagram

--- shared/src/main/scala/doodle/examples/BoxesAndArrows.scala
+++ shared/src/main/scala/doodle/examples/BoxesAndArrows.scala
@@ -14,20 +14,23 @@ object BoxesAndArrows {
 
   val spacer = rectangle(size * 0.2,size).noFill.noLine
 
-  val box = roundedRectangle(size, size, size*.12).lineWidth(size*.12).noFill 
+  val box = roundedRectangle(size, size, size*.12).lineWidth(size*.12).noFill
 
   val font = Font(SansSerif, Normal, Points(size/2))
   val equals = text("=").font(font)
 
-  val c = circle(size * 0.3)
-  val t = triangle(size * 0.6, size * 0.6)
+  val c = circle(size * 0.3).fillColor(Color.black)
+  val t = triangle(size * 0.6, size * 0.6).fillColor(Color.black)
 
   val circleBox = box on c
   val triangleBox = box on t
   val circleAndTriangleBox = box on (circle(size * 0.15) beside triangle(size * 0.3, size * 0.3))
 
   val circleToTriangle =
-    c beside spacer beside rightArrow(size, size) beside spacer beside t
+    c beside spacer beside rightArrow(size, size).fillColor(Color.black) beside spacer beside t
+
+  val circleToTriangleBox =
+    c beside spacer beside rightArrow(size, size).fillColor(Color.black) beside spacer beside triangleBox
 
   def besideWithSpace(elts: List[Image]): Image =
     elts.foldLeft(Image.empty){ (accum, elt) =>
@@ -45,6 +48,13 @@ object BoxesAndArrows {
     besideWithSpace(
       List(
         circleBox, text("|@|").font(font), triangleBox, equals, circleAndTriangleBox
+      )
+    )
+
+  val flatMap: Image =
+    besideWithSpace(
+      List(
+        circleBox, text("flatMap").font(font), circleToTriangleBox, equals, triangleBox
       )
     )
 }
