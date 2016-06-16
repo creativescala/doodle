7ea3ff77817f6d574b2841f37be79c5ff8cb669e
Noel Welsh <noel@Noels-MacBook-Pro.local>
2016-06-16 15:12:25 +0200

    Convenience constructors for elements with polar points

--- shared/src/main/scala/doodle/core/PathElements.scala
+++ shared/src/main/scala/doodle/core/PathElements.scala
@@ -6,25 +6,38 @@ object PathElement {
     MoveTo(point)
 
   def moveTo(x: Double, y: Double): PathElement =
-    MoveTo(Point.cartesian(x,y))
+    moveTo(Point.cartesian(x,y))
+
+  def moveTo(r: Double, angle: Angle): PathElement =
+    moveTo(Point.polar(r,angle))
 
 
   def lineTo(point: Point): PathElement =
     LineTo(point)
 
   def lineTo(x: Double, y: Double): PathElement =
-    LineTo(Point.cartesian(x,y))
+    lineTo(Point.cartesian(x,y))
 
+  def lineTo(r: Double, angle: Angle): PathElement =
+    lineTo(Point.polar(r,angle))
 
   def curveTo(cp1: Point, cp2: Point, to: Point): PathElement =
     BezierCurveTo(cp1, cp2, to)
 
   def curveTo(cp1X: Double, cp1Y: Double, cp2X: Double, cp2Y: Double, toX: Double, toY: Double): PathElement =
-    BezierCurveTo(
+    curveTo(
       Point.cartesian(cp1X, cp1Y),
       Point.cartesian(cp2X, cp2Y),
       Point.cartesian(toX,  toY)
     )
+
+  def curveTo(cp1R: Double, cp1Angle: Angle, cp2R: Double, cp2Angle: Angle, toR: Double, toAngle: Angle): PathElement =
+    curveTo(
+      Point.polar(cp1R, cp1Angle),
+      Point.polar(cp2R, cp2Angle),
+      Point.polar(toR,  toAngle)
+    )
+
 }
 final case class MoveTo(to: Point) extends PathElement
 final case class LineTo(to: Point) extends PathElement
