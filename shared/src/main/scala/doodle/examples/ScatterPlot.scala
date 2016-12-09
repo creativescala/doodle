3d485df007f24ac5f31226b7808624e6b1737516
Noel Welsh <noel@untyped.com>
2016-12-09 17:25:39 +0000

    Add example of circle plot

--- shared/src/main/scala/doodle/examples/ScatterPlot.scala
+++ shared/src/main/scala/doodle/examples/ScatterPlot.scala
@@ -3,6 +3,7 @@ package examples
 
 import doodle.core._
 import doodle.random._
+import doodle.syntax._
 import doodle.chart._
 import doodle.chart.syntax._
 import doodle.chart.StandardInterpreter._
@@ -11,11 +12,19 @@ import cats.syntax.traverse._
 import cats.syntax.cartesian._
 
 object ScatterPlot {
-  val point: Random[Point] =
-    (Random.normal(0.0, 50.0) |@| Random.normal(0.0, 50.0)) map { (x,y) => Point(x,y) }
+  object normal {
+    val point: Random[Point] =
+      (Random.normal(0.0, 50.0) |@| Random.normal(0.0, 50.0)) map { (x,y) => Point(x,y) }
 
-  val data: Random[List[Point]] =
-    List.range(0, 100).map(_ => point).sequence
+    val data: Random[List[Point]] =
+      List.range(0, 100).map(_ => point).sequence
 
-  val image = data.map(d => Chart.scatterPlot(d.asSeries)).run.asImage
+    val image = data.map(d => Chart.scatterPlot(d.asSeries)).run.asImage
+  }
+
+  object circle {
+    val data: List[Point] =
+      (for(a <- 0 to 360 by 5) yield { Point(1.0, a.degrees) }).toList
+    val image = Chart.scatterPlot(data.asSeries).asImage
+  }
 }
