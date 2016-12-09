b1326433a76b06f5fde36582ffdb9e1ab9928c89
Noel Welsh <noel@untyped.com>
2016-12-09 17:23:22 +0000

    Add `draw` and `save` syntax to `Chart`

--- shared/src/main/scala/doodle/chart/syntax/ChartSyntax.scala
+++ shared/src/main/scala/doodle/chart/syntax/ChartSyntax.scala
@@ -3,10 +3,22 @@ package chart
 package syntax
 
 import doodle.core.Image
+import doodle.backend.{Configuration, Draw, Save}
+import doodle.backend.{Interpreter => BackendInterpreter}
 
 trait ChartSyntax {
   implicit class ChartOps[A](chart: Chart) {
     def asImage(implicit i: Interpreter): Image =
       i(chart)
+
+    def draw(implicit ci: Interpreter, d: Draw, ii: Configuration => BackendInterpreter): Unit = {
+      import doodle.syntax._
+      chart.asImage.draw
+    }
+
+    def save[Format](fileName: String)(implicit ci: Interpreter, save: Save[Format], ii: Configuration => BackendInterpreter): Unit = {
+      import doodle.syntax._
+      chart.asImage.save[Format](fileName)
+    }
   }
 }
