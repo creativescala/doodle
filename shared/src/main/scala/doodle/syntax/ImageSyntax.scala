fb200774cc383625b6ffd5ce8dc327c39cff6f04
Noel Welsh <noel@untyped.com>
2016-12-09 17:25:06 +0000

    Syntax to `finalise` an `Image`
    Useful for debugging.

--- shared/src/main/scala/doodle/syntax/ImageSyntax.scala
+++ shared/src/main/scala/doodle/syntax/ImageSyntax.scala
@@ -1,12 +1,15 @@
 package doodle.syntax
 
-import doodle.core.Image
-import doodle.backend.{Draw, Interpreter, Configuration, Save}
+import doodle.core.{DrawingContext, Image}
+import doodle.backend.{Draw, Finalised, Interpreter, Configuration, Save}
 
 trait ImageSyntax {
   implicit class ToImageOps(val image: Image) {
     def draw(implicit draw: Draw, interpreter: Configuration => Interpreter): Unit =
       draw.draw(interpreter, image)
+
+    def finalise: Finalised =
+      Finalised.finalise(image, DrawingContext.blackLines)
 
     def save[Format](fileName: String)(implicit save: Save[Format], interpreter: Configuration => Interpreter): Unit =
       save.save[Format](fileName, interpreter, image)
