1b26770a2a4c32ba02d104d61cf54516dfd07bac
Noel Welsh <noel@untyped.com>
2017-05-31 17:56:28 +0200

    Allow direct drawing on the Canvas
    `Image.draw` allows the user to provide a method that draws directly on the
    canvas. This is generally faster than constructing an `Image` graph representing
    an equivalent output and is the only way to render images with millions of
    components. It is also arguably more suitable for certain types of output (e.g
    images where there is little structure.)
    
    Closes #43

--- js/src/main/scala/doodle/js/SvgCanvas.scala
+++ js/src/main/scala/doodle/js/SvgCanvas.scala
@@ -7,24 +7,44 @@ import doodle.backend.{BoundingBox, Canvas}
 
 import org.scalajs.dom
 
-final case class SvgCanvas(root: dom.svg.G, center: Point, screenCenter: Point)
+final class SvgCanvas(root: dom.svg.G, center: Point, screenCenter: Point)
     extends Canvas {
   import scalatags.JsDom.styles.{font => cssFont}
   import scalatags.JsDom.{svgTags => svg}
   import scalatags.JsDom.svgAttrs
   import scalatags.JsDom.implicits._
 
+  var transforms: List[String] = List.empty
+
+  def pushTransform(tx: Transform): Unit = {
+    val svg = Svg.toSvgTransform(tx)
+    transforms = svg :: transforms
+  }
+
+  def popTransform(): Unit = {
+    transforms match {
+      case hd :: tl =>
+        transforms = tl
+      case Nil =>
+        // Do nothing
+        ()
+    }
+  }
+
+  def currentTransform =
+    svgAttrs.transform:=(transforms.reverse.mkString)
+
   def closedPath(context: DrawingContext, elements: List[PathElement]): Unit = {
     val dAttr = Svg.toSvgPath(elements) ++ "Z"
     val style = Svg.toStyle(context)
-    val elt = svg.path(svgAttrs.style:=style, svgAttrs.d:=dAttr).render
+    val elt = svg.path(currentTransform, svgAttrs.style:=style, svgAttrs.d:=dAttr).render
     root.appendChild(elt)
   }
 
   def openPath(context: DrawingContext, elements: List[PathElement]): Unit = {
     val dAttr = Svg.toSvgPath(elements)
     val style = Svg.toStyle(context)
-    val elt = svg.path(svgAttrs.style:=style, svgAttrs.d:=dAttr).render
+    val elt = svg.path(currentTransform, svgAttrs.style:=style, svgAttrs.d:=dAttr).render
     root.appendChild(elt)
   }
 
@@ -38,11 +58,12 @@ final case class SvgCanvas(root: dom.svg.G, center: Point, screenCenter: Point)
       // bounding box origin is at the center of the text.
       val bottomLeft = Transform.translate(-boundingBox.width/2, -boundingBox.height/2)
       val fullTx = Transform.horizontalReflection andThen tx andThen bottomLeft
+      val svgTx = transforms.reverse.mkString ++ " " ++ Svg.toSvgTransform(fullTx)
       val font = FontMetrics.toCss(f)
       val elt = svg.text(svgAttrs.style:=style,
                          svgAttrs.x:=0,
                          svgAttrs.y:=0,
-                         svgAttrs.transform:=Svg.toSvgTransform(fullTx),
+                         svgAttrs.transform:=svgTx,
                          cssFont:=font,
                          characters).render
       root.appendChild(elt)
