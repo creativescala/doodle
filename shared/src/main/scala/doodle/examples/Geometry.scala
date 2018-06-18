243f3a7318ac22d469d960ccaab1622c96c6c5fd
Noel Welsh <noel@untyped.com>
2018-06-18 14:42:15 -0400

    Miscellaneous examples

---
+++ shared/src/main/scala/doodle/examples/Geometry.scala
@@ -0,0 +1,20 @@
+import doodle.core._
+import doodle.syntax._
+import doodle.turtle._
+import doodle.turtle.Instruction._
+
+object Geometry {
+  val instructions =
+    List(
+      forward(100), turn(45.degrees),
+      forward(100), turn(45.degrees),
+      forward(100), turn(45.degrees),
+      forward(100), turn(45.degrees),
+      forward(100), turn(45.degrees),
+      forward(100), turn(45.degrees),
+      forward(100), turn(45.degrees),
+      forward(100)
+    )
+
+  val image = Turtle.draw(instructions).fillColor(Color.red)
+}
