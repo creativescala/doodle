6bf8cea05fc402cb07ce9bcada683605dfde8a25
Noel Welsh <noel@untyped.com>
2018-12-14 20:13:09 +0000

    Smart constructors for gradients
    Also some small cleanup

--- core/src/main/scala/doodle/core/Gradient.scala
+++ core/src/main/scala/doodle/core/Gradient.scala
@@ -1,8 +1,7 @@
 package doodle
 package core
 
-sealed trait Gradient extends Product with Serializable {}
-
+sealed trait Gradient extends Product with Serializable
 object Gradient {
   final case class Linear(start: Point,
                           end: Point,
@@ -16,30 +15,39 @@ object Gradient {
                           cycleMethod: CycleMethod)
       extends Gradient
 
+  def linear(start: Point, end: Point, stops: Seq[(Color, Double)], cycleMethod: CycleMethod): Gradient =
+    Linear(start, end, stops, cycleMethod)
+
+  def radial(outer: Point, inner: Point, radius: Double, stops: Seq[(Color, Double)], cycleMethod: CycleMethod): Gradient =
+    Radial(outer, inner, radius, stops, cycleMethod)
+
   def dichromaticVertical(color1: Color, color2: Color, length: Double) =
     Linear(Point.zero,
            Point.Cartesian(0, length),
            List((color1, 0.0), (color2, 1.0)),
-           CycleMethod.Repeat)
+           CycleMethod.repeat)
 
   def dichromaticHorizontal(color1: Color, color2: Color, length: Double) =
     Linear(Point.zero,
            Point.Cartesian(length, 0),
            List((color1, 0.0), (color2, 1.0)),
-           CycleMethod.Repeat)
+           CycleMethod.repeat)
 
   def dichromaticRadial(color1: Color, color2: Color, radius: Double) =
     Radial(Point.zero,
            Point.zero,
            radius,
            List((color1, 0.0), (color2, 1.0)),
-           CycleMethod.Repeat)
+           CycleMethod.repeat)
 
   sealed trait CycleMethod extends Product with Serializable {}
-
   object CycleMethod {
     final case object NoCycle extends CycleMethod
     final case object Reflect extends CycleMethod
     final case object Repeat extends CycleMethod
+
+    val noCycle: CycleMethod = NoCycle
+    val reflect: CycleMethod = Reflect
+    val repeat: CycleMethod = Repeat
   }
 }
