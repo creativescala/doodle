c612b5e339b43a2039f2082d61caf2b81e286ff3
Noel Welsh <noel@untyped.com>
2019-07-13 22:22:27 +0100

    Make the `PulsingCircle` example more interesting
    I'm happy with this.

--- java2d/src/main/scala/doodle/java2d/examples/PulsingCircle.scala
+++ java2d/src/main/scala/doodle/java2d/examples/PulsingCircle.scala
@@ -28,39 +28,73 @@ object PulsingCircle {
 
   val frame = Frame.size(600, 600).background(Color.midnightBlue)
 
-  def circle(diameter: Int): Picture[Unit] =
-    Picture { implicit algebra =>
-      algebra
-        .circle(diameter.toDouble)
-        .strokeColor(Color.crimson)
-        .on(algebra
-              .circle((diameter - 9).toDouble)
-              .strokeColor(Color.crimson.spin(30.degrees)))
-        .on(algebra
-              .circle((diameter - 18).toDouble)
-              .strokeColor(Color.crimson.spin(60.degrees)))
-        .strokeWidth(3.0)
-        .noFill
+  val strokeWidth = 9.0
+  val gapWidth = 6.0
+  val minimumDiameter = gapWidth + strokeWidth
+  val maxNumberOfCircles = 15
+
+  def circle(count: Int): Picture[Unit] =
+    count match {
+      case 0 =>
+        Picture{ implicit algebra =>
+          algebra.circle(minimumDiameter.toDouble)
+            .noFill
+            .strokeWidth(strokeWidth)
+        }
+      case n =>
+        Picture{ implicit algebra =>
+          algebra.circle((n * 2 * (strokeWidth + gapWidth) + minimumDiameter))
+            .noFill
+            .strokeWidth(strokeWidth)
+        }
+    }
+
+  def background(count: Int): Picture[Unit] = {
+    def iter(count: Int): Picture[Unit] =
+      count match {
+        case 0 =>
+          circle(count)
+        case n =>
+          circle(count).on(iter(n - 1))
+      }
+
+    iter(count).strokeWidth(strokeWidth.toDouble).strokeColor(Color.darkGray)
+  }
+
+  def pulse(count: Int): Picture[Unit] =
+    count match {
+      case 0 => circle(0).strokeColor(Color.crimson)
+      case 1 =>
+        circle(1)
+          .strokeColor(Color.crimson)
+          .on(circle(0).strokeColor(Color.crimson.spin(30.degrees)))
+      case n =>
+        circle(n)
+          .strokeColor(Color.crimson)
+          .on(circle(n-1)
+                .strokeColor(Color.crimson.spin(30.degrees)))
+          .on(circle(n-2)
+                .strokeColor(Color.crimson.spin(60.degrees)))
     }
 
   val animation: Observable[Picture[Unit]] =
     Observable
       .repeat(1)
-      .scan((3, 18)) { (state, _) =>
-        val (inc, diameter) = state
-        if (diameter >= 500) (-3, diameter - 3)
-        else if (diameter <= 18) (3, diameter + 3)
-        else (inc, diameter + inc)
+      .scan((1, 0)) { (state, _) =>
+        val (inc, count) = state
+        if (count >= maxNumberOfCircles) (-1, maxNumberOfCircles - 1)
+        else if (count <= 0) (1, 1)
+        else (inc, count + inc)
       }
-      .map { case (_, d) => circle(d) }
+      .map { case (_, c) => pulse(c).on(background(maxNumberOfCircles)) }
 
   def go() =
     animation.animateFrames(frame)
 
 
-  import doodle.java2d.effect._
-  import java.io.File
-  println("About to save")
-  Java2dAnimationWriter.write(new File("pulsing-circle-2.gif"), Frame.size(600, 600), doodle.java2d.examples.PulsingCircle.animation.take(100)).unsafeRunSync()
-  println("Saved")
+  // import doodle.java2d.effect._
+  // import java.io.File
+  // println("About to save")
+  // Java2dAnimationWriter.write(new File("pulsing-circle-2.gif"), Frame.size(600, 600), doodle.java2d.examples.PulsingCircle.animation.take(112)).unsafeRunSync()
+  // println("Saved")
 }
