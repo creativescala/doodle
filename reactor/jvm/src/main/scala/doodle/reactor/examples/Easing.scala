4a9b89ac6dde0e8042b7b42e4133a4827777c720
Noel Welsh <noel@untyped.com>
2019-09-05 12:46:44 +0100

    Add Reactor project
    Reactors provide a simple way to create animations. Currently they only work
    with the Image library, and do not support input. In the future we might extend
    them to the core algebras and support input methods that the `interact` project
    provides.
    
    Inspired by the library of the same name in Pyret.

---
+++ reactor/jvm/src/main/scala/doodle/reactor/examples/Easing.scala
@@ -0,0 +1,29 @@
+package doodle
+package reactor
+package examples
+
+import doodle.core._
+import doodle.image.Image
+import doodle.java2d._
+
+object Easing {
+  def easeIn(t: Double): Double =
+    Math.pow(t, 2.0)
+
+  def easeOut(t: Double): Double =
+    1.0 - Math.pow(1.0 - t, 2.0)
+
+  def scale(min: Double, max: Double): Double => Double = {
+    val range = max - min
+    (x: Double) => min + (x * range)
+  }
+
+  val step = (easeIn _).andThen(scale(-300.0, 300.0))
+  val reactor =
+    Reactor
+      .linearRamp()
+      .render(t => Image.circle(5.0).fillColor(Color.seaGreen).at(step(t), 0.0))
+
+  def go() =
+    reactor.run(Frame.size(600, 600))
+}
