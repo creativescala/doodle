2c741976640ac24a80b41aab68c8d00f228d2519
Noel Welsh <noel@untyped.com>
2020-01-19 07:58:34 +0000

    Fix test

--- interact/shared/src/test/scala/doodle/interact/animation/InterpolationSpec.scala
+++ interact/shared/src/test/scala/doodle/interact/animation/InterpolationSpec.scala
@@ -80,7 +80,7 @@ object InterpolationSpec extends Properties("Interpolation properties") {
       .upToIncluding(start.toDouble + difference)
       .forSteps(difference.toLong + 1)
     difference match {
-      case 0 => t.toList ?= List.empty[Double]
+      case 0 => t.toList ?= List.(start.toDouble)
       case 1 => t.toList ?= List(start.toDouble + difference)
       case _ =>
         t.toList ?= List.tabulate(difference + 1) { a =>
