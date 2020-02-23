64e9b1bcd65009498a12ec978c123321b1b6d1d9
Noel Welsh <noel@untyped.com>
2020-02-23 17:38:26 +0000

    Make golden testing resilient to small errors
    Different platforms / JVMs render in slightly different ways that are
    not perceptually significant. Make golden testing robust against small
    errors by calculating the sum of squared error between two images and
    testing against a threshold.

--- golden/src/test/scala/doodle/golden/Golden.scala
+++ golden/src/test/scala/doodle/golden/Golden.scala
@@ -1,15 +1,31 @@
 package doodle
 package golden
 
-import doodle.syntax._
 import doodle.image._
 import doodle.image.syntax._
 import doodle.java2d._
 import doodle.effect.Writer._
+import javax.imageio.ImageIO
 import munit._
 
 trait Golden { self: FunSuite =>
   val goldenDir = "golden/src/test/golden"
+
+  def pixelSumOfSquaredError(a: Int, b: Int): Int = {
+    var error = 0
+    var i = 0
+    while(i < 4) {
+      val shift = i * 8
+      val mask = 0x000000FF << shift
+      val aValue = (a & mask) >> shift
+      val bValue = (b & mask) >> shift
+
+      error = error + ((aValue - bValue) * (aValue - bValue))
+
+      i = i + 1
+    }
+    error
+  }
 
   def assertGoldenImage(name: String, image: Image)(implicit loc: Location) = {
     import java.io.File
@@ -20,10 +36,28 @@ trait Golden { self: FunSuite =>
 
       try {
         image.write[Png](temp)
-        val (_, actual) = read[Algebra,Drawing](temp).base64[Png]()
-        val (_, expected) = read[Algebra,Drawing](file).base64[Png]()
+        val actual = ImageIO.read(temp)
+        val expected = ImageIO.read(file)
 
-        assertEquals(actual, expected)
+        assertEquals(actual.getHeight(), expected.getHeight(), s"Heights differ")
+        assertEquals(actual.getWidth(), expected.getWidth(), s"Widths differ")
+
+        // Sum of squared error
+        var error = 0.0
+        val threshold = actual.getHeight() * actual.getWidth() * 4
+
+        var x = 0
+        while(x < actual.getWidth()) {
+          var y = 0
+          while(y < actual.getHeight()) {
+            error = error + pixelSumOfSquaredError(actual.getRGB(x, y), expected.getRGB(x, y))
+
+            y = y + 1
+          }
+          x = x + 1
+        }
+
+        assert(clue(error) < clue(threshold), "Error greater than threshold")
       } finally {
         if(temp.exists()) temp.delete()
         ()
