a2d6762b8fbb5b5806735196a3e4e322de6efde3
Noel Welsh <noel@untyped.com>
2020-02-29 20:33:41 +0000

    Make text golden test more robust
    Fix the size of the images so they are more robust to minor differences
    in rendering across platforms.

--- golden/src/test/scala/doodle/golden/Text.scala
+++ golden/src/test/scala/doodle/golden/Text.scala
@@ -1,25 +1,32 @@
 package doodle
 package golden
 
+import cats.implicits._
 import doodle.core.font._
 import doodle.java2d._
 import doodle.syntax._
 import munit._
 
 class Text extends FunSuite with Golden {
+  // Enusre the text images have the same size, which guards to some extent
+  // against different font rendering on different machines
+  val spacer = square[Algebra, Drawing](100).noFill.noStroke
+
   testPicture("text-serif-default") {
-    text[Algebra, Drawing]("Hi!").font(Font.defaultSerif)
+    text[Algebra, Drawing]("Hi!").font(Font.defaultSerif).on(spacer)
   }
 
   testPicture("text-sans-serif-default") {
-    text[Algebra, Drawing]("Hi!").font(Font.defaultSansSerif)
+    text[Algebra, Drawing]("Hi!").font(Font.defaultSansSerif).on(spacer)
   }
 
   testPicture("text-serif-48pt") {
-    text[Algebra, Drawing]("Hi!").font(Font.defaultSerif.size(48))
+    text[Algebra, Drawing]("Hi!").font(Font.defaultSerif.size(48)).on(spacer)
   }
 
   testPicture("text-sans-serif-48pt") {
-    text[Algebra, Drawing]("Hi!").font(Font.defaultSansSerif.size(48))
+    text[Algebra, Drawing]("Hi!")
+      .font(Font.defaultSansSerif.size(48))
+      .on(spacer)
   }
 }
