39c2593ba7a9741fb3a1e3f36e457b12dabcdc32
Noel Welsh <noel@noelwelsh.com>
2021-10-08 16:13:58 +0100

    Reformat

--- reactor/jvm/src/main/scala/doodle/reactor/examples/Easing.scala
+++ reactor/jvm/src/main/scala/doodle/reactor/examples/Easing.scala
@@ -23,7 +23,9 @@ object Easing {
   val reactor =
     Reactor
       .linearRamp()
-      .withRender(t => Image.circle(5.0).fillColor(Color.seaGreen).at(step(t), 0.0))
+      .withRender(t =>
+        Image.circle(5.0).fillColor(Color.seaGreen).at(step(t), 0.0)
+      )
 
   def go() =
     reactor.run(Frame.size(600, 600))
