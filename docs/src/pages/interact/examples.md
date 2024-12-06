55f3eed95b93e4b9623cde1869be9229453e0f46
Noel Welsh <noel@noelwelsh.com>
2024-12-06 15:56:38 +0000

    Add another animation example

---
+++ docs/src/pages/interact/examples.md
@@ -0,0 +1,5 @@
+# Examnples
+
+These are miscellaneous examples of animations and interactivity.
+
+@:doodle("bouncy-circles", "SvgBouncyCircles.go")
---
+++ examples/js/src/main/scala/doodle/examples/svg/SvgBouncyCircles.scala
@@ -0,0 +1,72 @@
+package doodle.examples.svg
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import doodle.core.*
+import doodle.interact.easing.*
+import doodle.interact.syntax.all.*
+import doodle.svg.*
+import doodle.syntax.all.*
+import fs2.Stream
+
+import scala.concurrent.duration.*
+import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+@JSExportTopLevel("SvgBouncyCircles")
+object BouncyCircles {
+
+  @JSExport
+  def go(id: String) = {
+    val frame = Frame(id).withSize(600, 600).withBackground(Color.darkMagenta)
+    val steps = 60 * 10
+
+    def bounce(easing: Easing): Stream[IO, Double] =
+      easing.toStream(steps) ++
+        easing.toStream(steps) ++
+        easing.toStream(steps) ++
+        easing.toStream(steps)
+
+    val animation: Stream[IO, Picture[Unit]] =
+      bounce(Easing.linear)
+        .zip(bounce(Easing.quadratic))
+        .zip(bounce(Easing.cubic))
+        .zip(bounce(Easing.sin))
+        .zip(bounce(Easing.circle))
+        .zip(bounce(Easing.back))
+        .map { case (((((r1, r2), r3), r4), r5), r6) =>
+          circle(r1 * 85 + 10)
+            .strokeColor(Color.magenta.spin(180.degrees))
+            .at(r1 * 400 - 200, 250)
+            .on(
+              circle(r2 * 85 + 10)
+                .strokeColor(Color.magenta.spin(170.degrees))
+                .at(r2 * 400 - 200, 150)
+            )
+            .on(
+              circle(r3 * 85 + 10)
+                .strokeColor(Color.magenta.spin(140.degrees))
+                .at(r3 * 400 - 200, 50)
+            )
+            .on(
+              circle(r4 * 85 + 10)
+                .strokeColor(Color.magenta.spin(150.degrees))
+                .at(r4 * 400 - 200, -50)
+            )
+            .on(
+              circle(r5 * 85 + 10)
+                .strokeColor(Color.magenta.spin(140.degrees))
+                .at(r5 * 400 - 200, -150)
+            )
+            .on(
+              circle(r6 * 85 + 10)
+                .strokeColor(Color.magenta.spin(120.degrees))
+                .at(r6 * 400 - 200, -250)
+            )
+            .strokeWidth(2.0)
+        }
+        .evalTap(_ => IO.println("A frame"))
+
+    animation.withFrameRate(20.milliseconds).animate(frame)
+  }
+}
