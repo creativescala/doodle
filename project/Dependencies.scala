dcf5700e1d81bfd9c5c56a5da606850339e1e1bf
Noel Welsh <noel@noelwelsh.com>
2025-02-28 21:12:17 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport.*
 
 object Dependencies {
   // Library Versions
-  val catsVersion = "2.12.0"
+  val catsVersion = "2.13.0"
   val catsEffectVersion = "3.5.7"
   val fs2Version = "3.11.0"
 
