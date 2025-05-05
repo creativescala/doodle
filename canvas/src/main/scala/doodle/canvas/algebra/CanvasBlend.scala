e25cdbfb03b43b3ecaef3005695cc99677cc4428
Atharva-Kanerkar <atharvakanherkar25@gmail.com>
2025-05-05 23:43:40 +0530

    Formatting

--- canvas/src/main/scala/doodle/canvas/algebra/CanvasBlend.scala
+++ canvas/src/main/scala/doodle/canvas/algebra/CanvasBlend.scala
@@ -1,4 +1,4 @@
- /*
+/*
  * Copyright 2015 Creative Scala
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,35 +20,29 @@ package algebra
 
 import doodle.algebra.Blend
 import doodle.algebra.generic.Finalized
-import doodle.algebra.generic.Renderable  
+import doodle.algebra.generic.Renderable
 import cats.Eval
 import org.scalajs.dom
 
 trait CanvasBlend extends Blend {
   self: CanvasAlgebra =>
 
- 
   import self.Drawing
 
   private def blend[A](
       image: Drawing[A],
       blendMode: String
   ): Drawing[A] = {
-    Finalized { transform => 
-      image.run(transform).map { case (bb, rdr) =>  
-        // Create a new Renderable that wraps the original
+    Finalized { transform =>
+      image.run(transform).map { case (bb, rdr) =>
         val newRenderable: Renderable[CanvasDrawing, A] =
-          Renderable { tx => 
- 
-            val originalDrawingEval: Eval[CanvasDrawing[A]] = rdr.runA(tx) // Eval[CanvasRenderingContext2D => A]
+          Renderable { tx =>
+            val originalDrawingEval: Eval[CanvasDrawing[A]] = rdr.runA(tx)
 
-            // Map over the Eval to create the new CanvasDrawing function
-            originalDrawingEval.map { originalDrawing => // originalDrawing: CanvasRenderingContext2D => A
-              // Explicitly construct a CanvasDrawing using the companion object's apply method
-              canvas.algebra.CanvasDrawing.apply { (ctx: dom.CanvasRenderingContext2D) => // ctx: CanvasRenderingContext2D
+            originalDrawingEval.map { originalDrawing =>
+              CanvasDrawing { (ctx: dom.CanvasRenderingContext2D) =>
                 ctx.save()
                 ctx.globalCompositeOperation = blendMode
-                // Execute the original drawing function within the modified context
                 val result: A = originalDrawing(ctx)
                 ctx.restore()
                 result
@@ -60,7 +54,6 @@ trait CanvasBlend extends Blend {
     }
   }
 
- 
   def screen[A](image: Drawing[A]): Drawing[A] =
     blend(image, "screen")
 
