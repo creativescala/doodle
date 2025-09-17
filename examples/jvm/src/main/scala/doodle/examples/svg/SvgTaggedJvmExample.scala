b542f8c3fc28ad365a7923194ab4b876785ed705
Noel Welsh <noel@noelwelsh.com>
2025-09-17 20:28:26 +0100

    Fix and format

--- examples/jvm/src/main/scala/doodle/examples/svg/SvgTaggedJvmExample.scala
+++ examples/jvm/src/main/scala/doodle/examples/svg/SvgTaggedJvmExample.scala
@@ -1,12 +1,28 @@
+/*
+ * Copyright 2015 Creative Scala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package doodle.examples.svg
 
+import cats.effect.unsafe.implicits.global
 import doodle.core.Color
+import doodle.effect.DefaultFrame
 import doodle.svg.*
 import doodle.svg.effect.Frame
 import doodle.svg.syntax.all.*
 import doodle.syntax.all.*
-import cats.effect.unsafe.implicits.global
-import doodle.effect.DefaultFrame
 
 object SvgTaggedJvmExample {
 
